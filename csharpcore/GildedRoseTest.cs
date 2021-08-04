78964115440d78257e5c9ea6c75cb15530b7eb62
Dustin Roszell <dustin.roszell@homeinsteadinc.com>
2021-08-04 11:36:58 -0500

    Adds test to ensure Aged Brie increases in quality

--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -41,5 +41,14 @@ namespace csharpcore
             app.UpdateQuality();
             Items[0].Quality.Should().Be(50);
         }
+
+        [Fact]
+        public void AgedBrieIncreasesInQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1, Quality = 1 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Quality.Should().Be(2);
+        }
     }
 }
